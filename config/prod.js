a0f50c61258b42a2ea1037a4ab172c9eb6ce1cd3
Anthony Brown <awebrown@gmail.com>
2018-03-23 22:35:39 -0400

    fixed settings

--- config/prod.js
+++ config/prod.js
@@ -5,8 +5,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
-//
-// googleClientID: '946104132323-p8oege0oa4k827095a8knutkbm4ds01c.apps.googleusercontent.com',
-// googleClientSecret: 'u9cAiMs5QYRRBiQiJncyl6b0',
-// mongoURI: 'mongodb://awebrown:thurber14@ds123399.mlab.com:23399/emaily-dev',
-// cookieKey: 'rftgyhujifgh45678fghj'
