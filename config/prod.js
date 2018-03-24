e6028eb8086d12c0a404c5f9d8b2231fc881d749
Anthony Brown <awebrown@gmail.com>
2018-03-23 22:23:40 -0400

    auth flow

---
+++ config/prod.js
@@ -0,0 +1,12 @@
+//PROD
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+//
+// googleClientID: '946104132323-p8oege0oa4k827095a8knutkbm4ds01c.apps.googleusercontent.com',
+// googleClientSecret: 'u9cAiMs5QYRRBiQiJncyl6b0',
+// mongoURI: 'mongodb://awebrown:thurber14@ds123399.mlab.com:23399/emaily-dev',
+// cookieKey: 'rftgyhujifgh45678fghj'
