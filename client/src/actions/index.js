04afe95c841ce0b87d3d6a009aece9029de948ff
Anthony Brown <awebrown@gmail.com>
2018-03-29 13:53:49 -0400

    logs

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,6 +5,7 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => async dispatch => {
   console.log('[fetching user]');
     const res = await axios.get('/api/current_user');
+    console.log('[Fetched User Response]', res);
     dispatch({ type: FETCH_USER, payload: res.data});
 };
 
