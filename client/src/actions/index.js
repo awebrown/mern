ec0b5dc09392803dc4769a9f7858a75826ca34f3
Anthony Brown <awebrown@gmail.com>
2018-03-29 13:27:06 -0400

    syntax fix and logs

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,7 @@ export const fetchUser = () => async dispatch => {
 };
 
 export const handleToken = token => async dispatch => {
+  console.log('in actions index');
  const res = await axios.post('/api/stripe', token);
 
  dispatch({ type: FETCH_USER, payload: res.data});
